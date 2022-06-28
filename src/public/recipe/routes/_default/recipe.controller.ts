a0add8f344b8ea5b06e3ce9b348091079420f341
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-06-28 18:59:42 +0100

    Fixes

--- src/public/recipe/routes/_default/recipe.controller.ts
+++ src/public/recipe/routes/_default/recipe.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, InternalServerErrorException, NotFoundException, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { Injector } from '@nestjs/core/injector/injector';
 import { MedicationService } from 'src/public/medication/routes/__default/services/medication.service';
 import { MedicationModel } from 'src/public/medication/structs/medication.model';
@@ -13,94 +22,106 @@ import { RecipeService } from './services/recipe.service';
 
 @Controller(`${PublicRoute.RECIPES}`)
 export class RecipeController {
-    public constructor(
-        private readonly _recipeService: RecipeService<RecipeModel>,
-        private readonly _medicationService: MedicationService<MedicationModel>
-    ) {}
+  public constructor(
+    private readonly _recipeService: RecipeService<RecipeModel>,
+    private readonly _medicationService: MedicationService<MedicationModel>,
+  ) {}
 
-    @UseGuards(AccessTokenGuard)
-    @Get()
-    public async fetchRecipes(): Promise<Array<FetchResponseModel<RecipeModel>>> {
-        const recipes = await this._recipeService.fetchBulk().catch((error) => {
-            throw new InternalServerErrorException(error);
-        });
+  @UseGuards(AccessTokenGuard)
+  @Get()
+  public async fetchRecipes(): Promise<Array<FetchResponseModel<RecipeModel>>> {
+    const recipes = await this._recipeService.fetchBulk().catch((error) => {
+      throw new InternalServerErrorException(error);
+    });
 
-        const responses: Array<FetchResponseModel<RecipeModel>> = new Array();
+    const responses: Array<FetchResponseModel<RecipeModel>> = new Array();
 
-        for (const recipe of recipes) {
-            responses.push(new FetchResponseModel<RecipeModel>(
-                recipe.recipeId,
-                recipe
-            ));
-        }
-
-        return responses;
+    for (const recipe of recipes) {
+      responses.push(
+        new FetchResponseModel<RecipeModel>(recipe.recipeId, recipe),
+      );
     }
 
-    @UseGuards(AccessTokenGuard)
-    @Get("/:id")
-    public async fetchRecipesByCitizenId(@Param("id") citizenId: FetchRecipeByCitizenDTO): Promise<Array<FetchResponseModel<RecipeModel>>> {
-        const recipes = await this._recipeService.fetchBulkByHttpParam(citizenId).catch((error) => {
-            throw new InternalServerErrorException(error);
-        });
+    return responses;
+  }
 
-        const responses: Array<FetchResponseModel<RecipeModel>> = new Array();
+  @UseGuards(AccessTokenGuard)
+  @Get('/:id')
+  public async fetchRecipesByCitizenId(
+    @Param('id') citizenId: FetchRecipeByCitizenDTO,
+  ): Promise<Array<FetchResponseModel<RecipeModel>>> {
+    const recipes = await this._recipeService
+      .fetchBulkByHttpParam(citizenId)
+      .catch((error) => {
+        throw new InternalServerErrorException(error);
+      });
 
-        for (const recipe of recipes) {
-            responses.push(new FetchResponseModel<RecipeModel>(
-                recipe.recipeId,
-                recipe
-            ));
-        }
+    const responses: Array<FetchResponseModel<RecipeModel>> = new Array();
 
-        return responses;
+    for (const recipe of recipes) {
+      responses.push(
+        new FetchResponseModel<RecipeModel>(recipe.recipeId, recipe),
+      );
     }
 
-    @UseGuards(AccessTokenGuard)
-    @Post()
-    public async createRecipe(@Body() createRecipeDTO: CreateRecipeDTO): Promise<Array<CreateResponseModel<RecipeModel>>> {
-        const medications: Array<Omit<MedicationModel, "basePrice" | "createdAt" | "updatedAt">> = new Array<Omit<MedicationModel, "basePrice" & "createdAt" & "updatedAt">>();
+    return responses;
+  }
 
-        for(const medicationId of createRecipeDTO.medications) {
-            const med = await this._medicationService.fetchOne({medicationId: medicationId}).catch((error) => {
-                throw new InternalServerErrorException(error);
-            });
-            
-            const medication: Omit<MedicationModel, "basePrice" | "createdAt" | "updatedAt"> = {
-                medicationId: med.medicationId,
-                signature: med.signature,
-                alias: med.alias
-            }
-            
-            medications.push(medication)
-        }
+  @UseGuards(AccessTokenGuard)
+  @Post()
+  public async createRecipe(
+    @Body() createRecipeDTO: CreateRecipeDTO,
+  ): Promise<Array<CreateResponseModel<RecipeModel>>> {
+    const medications: Array<
+      Omit<MedicationModel, 'basePrice' | 'createdAt' | 'updatedAt'>
+    > = new Array<
+      Omit<MedicationModel, 'basePrice' & 'createdAt' & 'updatedAt'>
+    >();
 
-        const recipe = { 
-            citizenId: createRecipeDTO.citizenId,
-            userId: createRecipeDTO.userId,
-            medications: medications,
-            createdAt: new Date(createRecipeDTO.createdAt).toISOString(),
-        };
-
-        await this._recipeService.createOne(recipe).catch((error) => {
-            throw new InternalServerErrorException(error);
+    for (const medicationId of createRecipeDTO.medications) {
+      const med = await this._medicationService
+        .fetchOne({ medicationId: medicationId })
+        .catch((error) => {
+          throw new InternalServerErrorException(error);
         });
 
-        const recipes = await this._recipeService.fetchBulkByHttpParam(createRecipeDTO.citizenId).catch((error) => {
-            throw new InternalServerErrorException(error);
-        });
+      const medication: Omit<
+        MedicationModel,
+        'basePrice' | 'createdAt' | 'updatedAt'
+      > = {
+        medicationId: med.medicationId,
+        signature: med.signature,
+        alias: med.alias,
+      };
 
-        const responses: Array<CreateResponseModel<RecipeModel>> = new Array();
-
-        for (const recipe of recipes) {
-            responses.push(new CreateResponseModel<RecipeModel>(
-                recipe.recipeId,
-                null,
-                recipe
-            ));
-        }
-
-        return responses;
-
+      medications.push(medication);
     }
+
+    const recipe = {
+      citizenId: createRecipeDTO.citizenId,
+      userId: createRecipeDTO.userId,
+      medications: medications,
+      createdAt: new Date(createRecipeDTO.createdAt).toISOString(),
+    };
+
+    await this._recipeService.createOne(recipe).catch((error) => {
+      throw new InternalServerErrorException(error);
+    });
+
+    const recipes = await this._recipeService
+      .fetchBulkByHttpParam(createRecipeDTO.citizenId)
+      .catch((error) => {
+        throw new InternalServerErrorException(error);
+      });
+
+    const responses: Array<CreateResponseModel<RecipeModel>> = new Array();
+
+    for (const recipe of recipes) {
+      responses.push(
+        new CreateResponseModel<RecipeModel>(recipe.recipeId, null, recipe),
+      );
+    }
+
+    return responses;
+  }
 }
--- src/public/recipe/routes/_default/services/recipe.service.ts
+++ src/public/recipe/routes/_default/services/recipe.service.ts
@@ -1,27 +1,52 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { IRecipeService } from '../recipe.service.interface';
 
 @Injectable()
 export class RecipeService<T> implements IRecipeService<T> {
-    public constructor(
-        private readonly _httpService: HttpService
-    ) { }
+  public constructor(
+    private readonly _httpService: HttpService,
+    private readonly _configService: ConfigService,
+  ) {}
 
-    public async fetchBulk(): Promise<Array<T>> {
-        const axiosResponse = await firstValueFrom(this._httpService.get("http://localhost:8080/med-recipes/")).catch((error) => error);
+  public async fetchBulk(): Promise<Array<T>> {
+    const header = String(this._configService.get('API_KEY'));
+    const axiosResponse = await firstValueFrom(
+      this._httpService.get('http://localhost:8080/med-recipes/', {
+        headers: { 'api-key': header },
+      }),
+    ).catch((error) => {
+      return error;
+    });
 
-        return axiosResponse.data;
-    }
+    return axiosResponse.data;
+  }
 
-    public async fetchBulkByHttpParam<K>(httpParams: K): Promise<T[]> {
-        const axiosResponse = await firstValueFrom(this._httpService.get(`http://localhost:8080/med-recipes/${httpParams}`)).catch((error) => error);
-    
-        return axiosResponse.data;
-    }
+  public async fetchBulkByHttpParam<K>(httpParams: K): Promise<T[]> {
+    const header = String(this._configService.get('API_KEY'));
 
-    public async createOne<K>(httpBody: K): Promise<void> {
-        return await firstValueFrom(this._httpService.post(`http://localhost:8080/med-recipes/`, httpBody)).catch((error) => error);
-    };
+    const axiosResponse = await firstValueFrom(
+      this._httpService.get(`http://localhost:8080/med-recipes/${httpParams}`, {
+        headers: { 'api-key': header },
+      }),
+    ).catch((error) => error);
+
+    return axiosResponse.data;
+  }
+
+  public async createOne<K>(httpBody: K): Promise<void> {
+    const header = String(this._configService.get('API_KEY'));
+    console.log(header);
+
+    return await firstValueFrom(
+      this._httpService.post(`http://localhost:8080/med-recipes/`, httpBody, {
+        headers: { 'api-key': header },
+      }),
+    ).catch((error) => {
+      console.log(error);
+      return error;
+    });
+  }
 }
